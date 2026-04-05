e34b322be76c41f36f1f444dd51878d5d610d847
Christoffer Heien <Chriss4302@hotmail.com>
2026-04-05 17:59:54 +0200

    Add Terraform variables for resource configuration

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  default = "rg-terraform-vm"
+}
+
+variable "location" {
+  default = "West Europe"
+}
+
+variable "vm_name" {
+  default = "vm-terraform"
+}
+
+variable "vm_size" {
+  default = "Standard_B1s"
+}
+
+variable "admin_username" {
+  default = "azureuser"
+}
